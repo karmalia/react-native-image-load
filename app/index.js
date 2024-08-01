5fba3c0d9c225edd3272ae447e0e8f82a1348238
ismail <ismailsevgi95@gmail.com>
2024-08-01 23:55:57 +0300

    test

--- app/index.js
+++ app/index.js
@@ -1,65 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
-import { Text, View, StyleSheet, Image } from "react-native";
-
-import {
-  useFonts,
-  Poppins_100Thin,
-  Poppins_100Thin_Italic,
-  Poppins_200ExtraLight,
-  Poppins_200ExtraLight_Italic,
-  Poppins_300Light,
-  Poppins_300Light_Italic,
-  Poppins_400Regular,
-  Poppins_400Regular_Italic,
-  Poppins_500Medium,
-  Poppins_500Medium_Italic,
-  Poppins_600SemiBold,
-  Poppins_600SemiBold_Italic,
-  Poppins_700Bold,
-  Poppins_700Bold_Italic,
-  Poppins_800ExtraBold,
-  Poppins_800ExtraBold_Italic,
-  Poppins_900Black,
-  Poppins_900Black_Italic,
-} from "@expo-google-fonts/poppins";
+import { Text, View, Image } from "react-native";
 
 export default () => {
-  let [fontsLoaded] = useFonts({
-    Poppins_100Thin,
-    Poppins_100Thin_Italic,
-    Poppins_200ExtraLight,
-    Poppins_200ExtraLight_Italic,
-    Poppins_300Light,
-    Poppins_300Light_Italic,
-    Poppins_400Regular,
-    Poppins_400Regular_Italic,
-    Poppins_500Medium,
-    Poppins_500Medium_Italic,
-    Poppins_600SemiBold,
-    Poppins_600SemiBold_Italic,
-    Poppins_700Bold,
-    Poppins_700Bold_Italic,
-    Poppins_800ExtraBold,
-    Poppins_800ExtraBold_Italic,
-    Poppins_900Black,
-    Poppins_900Black_Italic,
-  });
-
-  let fontSize = 24;
-  let paddingVertical = 6;
-
-  if (!fontsLoaded) {
-    return <Text>Loading...</Text>;
-  } else {
-    return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Image
-          source={require("../assets/icon.png")}
-          style={{ width: 100, height: 100 }}
-          resizeMode="contain"
-        />
-      </View>
-    );
-  }
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "gray",
+      }}
+    >
+      {/* Not loading */}
+      <Image
+        source={require("../assets/favicon.png")}
+        style={{ width: 900, height: 900 }}
+        resizeMode="contain"
+      />
+    </View>
+  );
 };
